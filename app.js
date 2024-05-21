a5c7eb1a3104f81651b0c28b59e45cc5cab1b43b
Ogoigbe1 <ogoigbe12@gmail.com>
2024-05-21 18:45:35 +0100

    "Updated dependencies in package.json and .gitignore, added new dependencies, and modified app.js and package-lock.json files."

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
 const app = express();
 const env = process.env;
+const API = env.API_URL;
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.options('*', cors());
+
+const authRouter = require('./routes/auth');
+
+app.use(`${API}/`, authRouter);
+
+
 // Start the server
 // localhost >> 192.168.0.0
 const hostname = env.HOST;
 const port = env.PORT;
 
+mongoose
+  .connect(env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log('Connected to Database');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
