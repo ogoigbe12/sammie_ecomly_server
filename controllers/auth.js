ad91c62ea929d7d6a432e05aeda01d5a12c96dd1
Ogoigbe1 <ogoigbe12@gmail.com>
2024-05-21 21:49:02 +0100

    "Added error logging and implemented password reset functionality with OTP verification"

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,6 +30,7 @@ exports.register = async function (req, res) {
 
     return res.status(201).json(user);
   } catch (error) {
+    console.error(error);
     if (error.message.includes('email_1 dup key')) {
       return res.status(409).json({
         type: 'AuthError',
@@ -77,6 +78,7 @@ exports.login = async function (req, res) {
     user.passwordHash = undefined;
     return res.json({ ...user._doc, accessToken });
   } catch (error) {
+    console.error(error);
     return res.status(500).json({ type: error.name, message: error.message });
   }
 };
@@ -102,6 +104,7 @@ exports.verifyToken = async function (req, res) {
     if (!isValid) return res.json(false);
     return res.json(true);
   } catch (error) {
+    console.error(error);
     return res.status(500).json({ type: error.name, message: error.message });
   }
 };
@@ -131,8 +134,65 @@ exports.forgotPassword = async function (req, res) {
     );
     return res.json({ message: response });
   } catch (error) {
+    console.error(error);
     return res.status(500).json({ type: error.name, message: error.message });
   }
 };
-exports.verifyPasswordResetOTP = async function (req, res) {};
-exports.resetPassword = async function (req, res) {};
+exports.verifyPasswordResetOTP = async function (req, res) {
+  try {
+    const { email, otp } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+
+    if (
+      user.resetPasswordOtp !== +otp ||
+      Date.now() > user.resetPasswordOtpExpires
+    ) {
+      return res.status(401).json({ message: 'Invalid or expired OTP' });
+    }
+    user.resetPasswordOtp = 1;
+    user.resetPasswordOtpExpires = undefined;
+
+    await user.save();
+    return res.json({ message: 'OTP confirmed successfully.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ type: error.name, message: error.message });
+  }
+};
+exports.resetPassword = async function (req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((error) => ({
+      field: error.path,
+      message: error.msg,
+    }));
+    return res.status(400).json({ errors: errorMessages });
+  }
+  try {
+    const { email, newPassword } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+
+    if (user.resetPasswordOtp !== 1) {
+      return res
+        .status(401)
+        .json({ message: 'Confirm OTP before resetting password.' });
+    }
+
+    user.passwordHash = bcrypt.hashSync(newPassword, 8);
+    user.resetPasswordOtp = undefined;
+    await user.save();
+
+    return res.json({ message: 'Password reset successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ type: error.name, message: error.message });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -21,6 +21,16 @@ const validateUser = [
     .withMessage('Please enter a valid phone number'),
 ];
 
+const validatePassword = [
+  body('newPassword')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters')
+    .isStrongPassword()
+    .withMessage(
+      'Password must contain at least one uppercase, one lowercase, and one symbol.'
+    ),
+];
+
 router.post('/login', authController.login);
 
 router.post('/register', validateUser, authController.register);
@@ -31,6 +41,6 @@ router.post('/forgot-password', authController.forgotPassword);
 
 router.post('/verify-otp', authController.verifyPasswordResetOTP);
 
-router.post('/reset-password', authController.resetPassword);
+router.post('/reset-password', validatePassword, authController.resetPassword);
 
 module.exports = router;
