03b061b0fb50d99c5a21d9f4aaff347283e99277
Ogoigbe1 <ogoigbe12@gmail.com>
2024-05-22 15:53:24 +0100

    "Added authJwt and errorHandler middlewares, usersRouter, and express-jwt dependency"

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv/config');
+const authJwt = require('./middlewares/jwt');
+const errorHandler = require('./middlewares/error_handler');
 
 const app = express();
 const env = process.env;
@@ -13,10 +15,14 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(cors());
 app.options('*', cors());
+app.use(authJwt());
+app.use(errorHandler);
 
 const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
 
 app.use(`${API}/`, authRouter);
+app.use(`${API}/users`, usersRouter);
 
 
 // Start the server
